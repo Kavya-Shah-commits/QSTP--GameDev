f2802847f30c84f4731ad67753963a24e8a554e3
Kavya Jayraj Shah <kavyashah6564@gmail.com>
2025-07-27 22:56:36 +0530

    Add files via upload

---
+++ CarContoller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarContoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotationSpeed, moveSpeed;
+    public Transform carTransform;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Debug.Log("Horizontal:"+ horizontalMovement + "vertical:" + verticalMovement);
+
+        if(Mathf.Abs(horizontalMovement)> 0.01f)
+        {
+            carTransform.Rotate(0f,0f, -1 * rotationSpeed * horizontalMovement);
+        }
+        if(Mathf.Abs(verticalMovement)> 0.01f)
+        {
+            carTransform.Translate(0f,moveSpeed * verticalMovement,0f);
+        }
+    }
+}
